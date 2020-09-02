b42d92be460a1e075a982fde31ada1051fbf2a95
israel ermel <israelermel@gmail.com>
2020-09-02 13:15:15 -0300

    permitido ambos iOS e macOS , todas layers

---
+++ iOSUiTests/SignUpViewControllerTests.swift
@@ -0,0 +1,18 @@
+//
+//  iOSUiTests.swift
+//  iOSUiTests
+//
+//  Created by Israel Ermel on 02/09/20.
+//  Copyright © 2020 Israel Ermel. All rights reserved.
+//
+
+import XCTest
+@testable import iOSUi
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() throws {
+       
+    }
+
+}
